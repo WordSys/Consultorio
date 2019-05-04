0a1ce7fe35cb6f6d1bc9e89e5c35363ff7df85fa
Walter Ordoñez <32498310+WordSys@users.noreply.github.com>
2019-05-03 22:01:30 -0300

    reparando commonController

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ConsultaController extends AbstractController
+class CommonController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
